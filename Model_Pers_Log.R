409dd5f13f8589a3aaaa89768069baa62c122c35
Quresh Latif <qureshlatif@yahoo.com>
2016-10-03 15:56:05 -0600

    Additional models
    One with shrubs and another with logging only. Looking like these will be the final models presented in the chapter.

---
+++ Model_Pers_Log.R
@@ -0,0 +1,172 @@
+library(R2jags)
+library(abind) # Don't think this is used, but left here just in case.
+setwd("F:/research stuff/FS_PostDoc/outside_consult/LoggingData_Julia")  # Set workspace to location with 'Data_compiled' workspace.
+#setwd("~/Documents/Rdata/LoggingData_Julia")
+load("Data_compiled.RData")
+
+# Extract and scale (z-score) covariate inputs #
+Logging <- (Plot.data$Logging - mean(Plot.data$Logging))/sd(Plot.data$Logging)
+
+nplot <- length(plot)
+nyear <- length(year)
+nspec <- length(spp)
+Y <- Y.arry  # Renamed to accommodate model code copied from previous project.
+
+Z.init <- apply(Y,c(1,2,3),function(x) sum(x>=1)) # Initial values for occupancy state parameter (only one really needed).
+
+# Bin assignments for finite-sample estimates #
+Unlogged <- as.numeric(Logging==min(Logging))
+Logged <- as.numeric(Logging>min(Logging))
+
+# Assemble the data names list for JAGS #
+data <- list("Y","nplot","nyear","n.visits","nspec","Logging","Unlogged","Logged")
+
+# Assemble the initial values for JAGS.  Here z is the apparent occupancy state 
+
+inits <- function(){list(Z=Z.init,rho=runif(1,-1,1))}
+
+# Assemble the parameters vector for JAGS (What we want to track).
+parameters <- c("pspp","beta0","beta1","beta.Logging","psi.Unlogged","psi.Logged")
+
+sink("model.txt")
+cat("
+
+# The model.
+model{
+
+for(i in 1:nspec){
+  # Species-specific parameters for baseline occupancy (intercept) and related parameters for each location.
+  beta0[i] ~ dnorm(mu.beta0,tau.beta0)T(-10,10) # mean probability of occupancy for sites previously unoccupied (= colonization or gamma)
+
+  #Species-specific parameters for detection
+  mu.eta[i] <- mu.alpha0 + (rho*sd.alpha0/sd.beta0)*(beta0[i] - mu.beta0)      #sd.alpha0 = sd(detection), sd.beta0 = sd(occupancy)
+  eta[i] ~ dnorm(mu.eta[i], var.eta)T(-10,10) #logit(p) - incorporates correlation between occupancy and detection across species
+  logit(pspp[i]) <- eta[i]      	#species-specific detection probability
+
+  #Species-specific parameters for occupancy covariates in the northwest
+  beta.Logging[i] ~ dnorm(mu.beta.Logging, tau.beta.Logging)T(-10,10)
+
+  beta1[i] ~ dnorm(mu.beta1, tau.beta1)T(-10,10) # Persistence offset (added to beta0 if occupied in previous year) 
+  # colonization probability (gamma in literature) = expit(beta0)
+  # persistence probability (phi in literature) = expit(beta0 + beta1)
+  
+  #Model
+  psi0[i] ~ dunif(0,1) # Probability of occupancy in year prior to sampling   
+  for(j in 1:nplot){  # Loop for year 1 - uses z0 to calculate persistence offset 
+    z0[j,i] ~ dbern(psi0[i]) # Occupancy state in year prior to sampling
+    ###Model for occupancy probability in year 1 ###
+    logit(psi[j,i,1]) <- beta0[i] + beta1[i]*z0[j,i] + beta.Logging[i]*Logging[j]
+    ###Model for detection probability ###
+    Z[j,i,1] ~ dbern(psi[j,i,1]) # Occupancy state
+    pZ[j,i,1] <- pspp[i]*Z[j,i,1]  # Unconditional probability of detection
+    Y[j,i,1] ~ dbin(pZ[j,i,1],n.visits[1]) # Data model
+    #____________Bayesian GOF_________________________________
+    ynew[j,i,1] ~ dbin(pZ[j,i,1],n.visits[1])  #simulated new data y under model
+    
+    LLsim[j,i,1] <- (ynew[j,i,1]*log(pspp[i])+
+      (n.visits[1]-ynew[j,i,1])*log(1-pspp[i]))*Z[j,i,1]  #log-likelihood simulated data
+    LLdata[j,i,1]<- (Y[j,i,1]*log(pspp[i])+
+      (n.visits[1]-Y[j,i,1])*log(1-pspp[i]))*Z[j,i,1]    #log-likelihood observed data
+    #_________________________________________________________
+    for(t in 2:nyear){  # Loop through remaining years 2 and up - uses Z[,,t-1] to calculate persistence offset
+      ###Model for occupancy probability ###
+      logit(psi[j,i,t]) <- beta0[i] + beta1[i]*Z[j,i,(t-1)] + beta.Logging[i]*Logging[j]
+      ###Model for detection probability ###
+      Z[j,i,t] ~ dbern(psi[j,i,t]) # Occupancy state
+      pZ[j,i,t] <- pspp[i]*Z[j,i,t] # Unconditional probability of detection
+      Y[j,i,t] ~ dbin(pZ[j,i,t],n.visits[t]) # Data model
+      #____________Bayesian GOF_________________________________
+      ynew[j,i,t] ~ dbin(pZ[j,i,t],n.visits[t])  #simulated data y under model
+    
+      LLsim[j,i,t] <- (ynew[j,i,t]*log(pspp[i])+
+        (n.visits[t]-ynew[j,i,t])*log(1-pspp[i]))*Z[j,i,t]  #log-likelihood simulated data
+      LLdata[j,i,t]<- (Y[j,i,t]*log(pspp[i])+
+        (n.visits[t]-Y[j,i,t])*log(1-pspp[i]))*Z[j,i,t]    #log-likelihood observed data
+      #_________________________________________________________
+      }
+      #___________Zs for finite-sample estimates________________
+      for(t in 1:nyear) {
+      Z.unlogged[j,i,t] <- Z[j,i,t]*Unlogged[j]
+      Z.logged[j,i,t] <- Z[j,i,t]*Logged[j]
+      #_________________________________________________________
+      }
+    }
+  # Derive finite-sample occupancy estimates #
+  psi.hat[i] <- sum(Z[,i,])/(nplot*nyear)
+  
+  psi.Unlogged[i] <- sum(Z.unlogged[,i,])/(sum(Unlogged[])*nyear)
+  psi.Logged[i] <- sum(Z.logged[,i,])/(sum(Logged[])*nyear)
+  }
+
+for(j in 1:nplot){  # Derive species richness estimates
+  for(t in 1:nyear){
+    SR[j,t] <- sum(Z[j,,t])
+  }
+}
+
+### Hyper-parameters for detection model ###
+
+mu.alpha0 ~ dnorm(0,0.1)
+tau.alpha0 <- 1/(sd.alpha0*sd.alpha0)
+sd.alpha0 ~ dunif(0,10)
+
+### Hyper-parameters for occupancy model ###
+
+mu.beta0 ~ dnorm(0,0.1)         #Hyper-parameter for intercept
+sd.beta0 ~ dunif(0,10)
+tau.beta0 <- 1/(sd.beta0*sd.beta0)
+
+mu.beta1 ~ dnorm(0,0.1)         #Hyper-parameter for persistence offset
+sd.beta1 ~ dunif(0,10)
+tau.beta1 <- 1/(sd.beta1*sd.beta1)
+
+mu.beta.Logging ~ dnorm(0,0.1)
+tau.beta.Logging <- 1/(sd.beta.Logging*sd.beta.Logging)
+sd.beta.Logging ~ dunif(0,10)
+
+rho ~ dunif(-1,1)				#correlation parameter relating occupancy with detection
+var.eta <- tau.alpha0/(1.-pow(rho,2))  #variance in detection
+
+#________Bayesian GOF___________
+#deviance
+dev_sim <- (-2)*sum(LLsim[,,])
+dev_data <- (-2)*sum(LLdata[,,])
+
+#test statistics should be ~0.5 if model fits
+test<-step(dev_data-dev_sim)
+#________________________________
+
+}
+",fill=TRUE)
+sink()
+
+# MCMC values.
+nc <- 4
+nb <- 5000
+ni <- 45000
+nt <- 1
+
+# Send it all to JAGS and hope for the best!
+
+# To help track time.
+starttime <- Sys.time()
+
+bugout <- jags(data, inits, parameters, model.file="model.txt", n.chains=nc, n.iter=ni,
+n.burnin=nb, n.thin=nt)
+
+endtime <- Sys.time()
+runtime <- endtime - starttime
+runtime  # Total time it took to run model.
+
+#Check n.effectives and R.hats for parameters
+length(which(bugout$BUGSoutput$summary[,"n.eff"]<100))/length(bugout$BUGSoutput$summary[,"n.eff"])
+min(bugout$BUGSoutput$summary[,"n.eff"])
+sort(bugout$BUGSoutput$summary[,"n.eff"])[1:50]
+max(bugout$BUGSoutput$summary[,"Rhat"])
+sort(bugout$BUGSoutput$summary[,"Rhat"],decreasing=T)[1:50]
+
+#use bugout object to manipulate results in R environment
+
+#save results
+library(R.utils)
+saveObject(bugout,"Mod_Pers_Log")
